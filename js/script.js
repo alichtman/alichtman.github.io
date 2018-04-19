45c54b3822131baf0d48c155fbb29d4c5edd9d72
alichtman <aaronlichtman@gmail.com>
2018-04-19 07:15:11 -0500

    Started adding in responsive drawer

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 function open_drawer() {
     console.log("open_drawer() called.");
-    var drawer = document.getElementById("sliding-drawer").style.marginRight = 475px;
+    var drawer = document.getElementById("sliding-drawer");
+    drawer.style.marginRight = "475px";
+    drawer.style.width = "500px";
+    drawer.style.top = "0";
+    drawer.style.right = "0";
+
+    var button = document.getElementById("drawer-button");
+    button.style.marginRight = "475px";
+    button.style.boxShadow = "10px 0 40px 1px rgba(0, 0, 0, 0.4)";
+
+//    Switch images after translation of -180 degrees
+//    Gonna need to mess with the margins of the button too
+
 }
 
 function close_drawer() {
+    /**
+     * Reset all css properties back to normal for the button, drawer, arrows. Nice transition back to left arrows.
+     */
     console.log("close_drawer() called.");
-    var drawer = document.getElementById("sliding-drawer");
-    drawer.style.color = "transparent";
+
     // Should reset to defaults
-    drawer.style.color=""
+    document.getElementById("sliding-drawer").style.color = "";
+    document.getElementById("drawer-button").style.color = "";
 
 //NEEDS TO BE CONFIGURED TO DO THIS
 //    #about-circle-button {
