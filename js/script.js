7cbca05c475ad8e2bcb289a68f8592aab0dc046d
alichtman <aaronlichtman@gmail.com>
2018-04-26 15:05:01 -0500

    tbh not sure exactly what I did but it's better now

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 function open_drawer() {
-    console.log("open_drawer() called.");
     var drawer = document.getElementById("sliding-drawer");
     drawer.style.width = "600px";
     document.getElementById("closed-drawer-button").style.display = "none";
@@ -8,8 +7,6 @@ function open_drawer() {
 }
 
 function close_drawer() {
-    console.log("close_drawer() called.");
-
     // Should reset to defaults
     document.getElementById("sliding-drawer").style.width = "";
     document.getElementById("opened-drawer-button").style.display = "none";
